de0589144f4b60acacfb75d11d5d6b1b95e86e95
cookingBird <86037230+cookingBird@users.noreply.github.com>
2023-11-22 22:54:34 +0800

    init project

---
+++ src/main/java/com/example/springbootstudyv2/SpringBootStudyV2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudyv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootstudyv2/SpringBootStudyV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudyv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
